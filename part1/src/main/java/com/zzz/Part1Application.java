3ff75faccdd5565cd211adb4edb0be81391e3392
zhangzezhai <361253659@qq.com>
2020-10-22 10:53:30 +0800

    Create Part1Application.java
    Co-Authored-By: zhangzezhai <16411852+zhangzezhai@users.noreply.github.com>

---
+++ part1/src/main/java/com/zzz/Part1Application.java
@@ -0,0 +1,14 @@
+package com.zzz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Part1Application {
+
+    //程序入口函数
+    public static void main(String[] args) {
+        SpringApplication.run(Part1Application.class, args);
+    }
+
+}

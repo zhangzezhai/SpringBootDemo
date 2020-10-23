88de5866260a04dfc1ac4d7ccfedabf5edab8bd5
zhangzezhai <361253659@qq.com>
2020-10-23 10:47:22 +0800

    springboot系列：（2）配置文件解析

---
+++ part2/src/main/java/com/zzz/Part2Application.java
@@ -0,0 +1,13 @@
+package com.zzz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Part2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Part2Application.class, args);
+    }
+
+}
---
+++ part2/src/test/java/com/zzz/Part2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zzz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Part2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

8aa069eabd988ef71599890ec43f9468393a4d8f
zhangzezhai <361253659@qq.com>
2020-10-28 10:45:28 +0800

    SpringBoot系列：（四）开发Web应用之Thymeleaf篇

---
+++ part4/src/main/java/com/zzz/Part4Application.java
@@ -0,0 +1,13 @@
+package com.zzz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Part4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Part4Application.class, args);
+    }
+
+}
---
+++ part4/src/test/java/com/zzz/Part4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zzz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Part4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
